5782e4b999b09ccbfdd56664da9ba8d4efd1f0c5
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-01-22 10:16:39 -0700

    random number function

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 # Ms.LaRose and Vienna creating a thing
+import random
 
+def rand_num():
+    num = random.randint(1,100)
+    return num
 
 def helloWorld(name):
     return f"Hello {name}!"
@@ -9,6 +13,7 @@ def main():
     print("Welcome to my program, please type your name: \n")
     name = input()
     print(helloWorld(name))
+    print(rand_num())
 
 
 main()
